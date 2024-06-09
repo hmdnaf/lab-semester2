5540898e30bc191037a4c2feebf8f64bf6313143
Hamdan AF <110399823+hmdnaf@users.noreply.github.com>
2024-06-10 00:58:42 +0800

    Add files via upload

---
+++ tugas 3/boolean1.js
@@ -0,0 +1,9 @@
+function checkBoolean(a, b) {
+    return a || b;
+}
+
+// Contoh penggunaan fungsi:
+let a = true;
+let b = false;
+let result = checkBoolean(a, b);
+console.log(result); // Output: true
---
+++ tugas 3/boolean3.js
@@ -0,0 +1,8 @@
+function hasilPerbandingan(a, b) {
+    return a === b;
+}
+
+let a = 5;
+let b = "5";
+let result = hasilPerbandingan(a, b);
+console.log(result); // Output: false
