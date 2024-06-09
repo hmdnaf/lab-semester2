56f3e4258007a54c1fd9d1ff279a802979edad10
Hamdan AF <110399823+hmdnaf@users.noreply.github.com>
2024-06-10 00:59:47 +0800

    Delete tugas 3 directory

--- tugas 3/boolean1.js
+++
@@ -1,9 +0,0 @@
-function checkBoolean(a, b) {
-    return a || b;
-}
-
-// Contoh penggunaan fungsi:
-let a = true;
-let b = false;
-let result = checkBoolean(a, b);
-console.log(result); // Output: true
--- tugas 3/boolean3.js
+++
@@ -1,8 +0,0 @@
-function hasilPerbandingan(a, b) {
-    return a === b;
-}
-
-let a = 5;
-let b = "5";
-let result = hasilPerbandingan(a, b);
-console.log(result); // Output: false
